a5870eb7a8ba3239703061ca58ab5579965761b3
Terje Io <terjeio@online.no>
2019-07-25 13:50:30 +0200

    Added I2C keypad plugin
    Required driver updates to follow for some drivers

---
+++ plugins/I2C keypad/keypad.h
@@ -0,0 +1,60 @@
+/*
+  keypad.h - I2C keypad plugin
+
+  Part of Grbl
+
+  Copyright (c) 2017-2019 Terje Io
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _KEYPAD_H_
+#define _KEYPAD_H_
+
+#include "driver.h"
+
+#define KEYBUF_SIZE 16 // must be a power of 2
+#define KEYPAD_I2CADDR 0x49
+
+#define JOG_XR   'R'
+#define JOG_XL   'L'
+#define JOG_YF   'F'
+#define JOG_YB   'B'
+#define JOG_ZU   'U'
+#define JOG_ZD   'D'
+#define JOG_XRYF 'r'
+#define JOG_XRYB 'q'
+#define JOG_XLYF 's'
+#define JOG_XLYB 't'
+#define JOG_XRZU 'w'
+#define JOG_XRZD 'v'
+#define JOG_XLZU 'u'
+#define JOG_XLZD 'x'
+
+typedef enum {
+    JogMode_Fast = 0,
+    JogMode_Slow,
+    JogMode_Step
+} jogmode_t;
+
+typedef void (*keycode_callback_ptr)(const char c);
+
+void process_keypress (uint_fast16_t state);
+void keypad_keyclick_handler (bool keydown);
+
+bool keypad_setting (setting_type_t setting, float value, char *svalue);
+void keypad_settings_restore (uint8_t restore_flag);
+void keypad_settings_report (bool axis_settings, axis_setting_type_t setting_type, uint8_t axis_idx);
+
+#endif
--- plugins/README.md
+++ plugins/README.md
@@ -1,6 +1,6 @@
 ## grblHAL plugins
 
-grblHAL's HAL interface is based on function pointers that may be used to add functionality without any need to change the core grbl code. The may also be changed on the fly to redirect calls, eg. the SD-card interface utilizes this to temporarily redirect input from the serial stream to the SD card.
+grblHAL's HAL interface is based on function pointers that may be used to add functionality without any need to change the core grbl code. They may also be changed on the fly to redirect calls, eg. the SD-card interface utilizes this to temporarily redirect input from the serial stream to the SD card.
 
 NOTE: A plugin needs to be supported by the processor specific driver - as a minimum a initialization call has to be made. 
 
