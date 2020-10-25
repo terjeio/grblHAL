ebdac3fd98ba1dd4262cc9a4f8a038fd73298bfd
Terje Io <terjeio@online.no>
2020-10-25 08:14:26 +0100

    Brougth simulator up to date with core

--- drivers/Simulator/grbl_eeprom_extensions.h
+++ drivers/Simulator/grbl_eeprom_extensions.h
@@ -1,2 +1,4 @@
-void memcpy_to_eeprom_with_checksum(uint32_t destination, uint8_t *source, uint32_t size);
-bool memcpy_from_eeprom_with_checksum(uint8_t *destination, uint32_t source, uint32_t size);
+#include "grbl/nvs.h"
+
+nvs_transfer_result_t memcpy_to_eeprom(uint32_t destination, uint8_t *source, uint32_t size, bool with_checksum);
+nvs_transfer_result_t memcpy_from_eeprom(uint8_t *destination, uint32_t source, uint32_t size, bool with_checksum);
